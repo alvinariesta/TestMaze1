f0327f1a2815ee73869f2ee3ce4ae099d88aaa16
alvinariesta <alvinariesta@ymail.com>
2018-05-15 10:48:29 +0700

    Fixed bug

--- src/testmaze1/TestMaze1.java
+++ src/testmaze1/TestMaze1.java
@@ -12,22 +12,22 @@ public class TestMaze1 {
         System.out.print("Masukkan Input ukuran: ");
         size=input.nextInt();
         
-        for(x=1;x<=size;x++){
-            for(y=1;y<=size;y++){
-                if (x%2==0){
-                    if(y==1||y==size){System.out.print("@");}
-                    else{System.out.print(" ");}
-                    diff++;
-                }else{
-                    if((diff%2==1&&y==2)||(diff%2==0&&y==size-1)){                        
-                        System.out.print(" ");                                            
-                    }
-                    else{
-                        System.out.print("@");
-                    }
-                }                    
-            }
-            System.out.println("");
-        }
-    }    
+            for(x=1;x<=size;x++){
+                for(y=1;y<=size;y++){
+                    if (x%2==0){
+                        if(y==1||y==size){System.out.print("@");}
+                        else{System.out.print(" ");}
+                        if(y==size)diff++;
+                    }else{
+                        if((diff%2==1&&y==2)||(diff%2==0&&y==size-1)){                        
+                            System.out.print(" ");                                            
+                        }
+                        else{
+                            System.out.print("@");
+                        }
+                    }                    
+                }
+                System.out.println("");
+            }          
+    }
 }
